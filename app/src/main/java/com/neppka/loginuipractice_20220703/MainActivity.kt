763a593ae015abd0301730ca604a92d5c3ea4899
shchoi <gllapia@naver.com>
2022-07-03 12:40:59 +0900

    강사님 로직 추가 - 주석처리함 20220703 일

--- app/src/main/java/com/neppka/loginuipractice_20220703/MainActivity.kt
+++ app/src/main/java/com/neppka/loginuipractice_20220703/MainActivity.kt
@@ -22,8 +22,21 @@ class MainActivity : AppCompatActivity() {
 
 //        로그인 로직 실행
         loginBtn.setOnClickListener {
+//            아이디 값과 비번 값 변수화
             val id = idEdt.text.toString()
             val pwd = passEdt.text.toString()
+
+//        1. 아이디값이 "admin", 비번 "adsf"
+//        2. 아이디 값 판별
+//            if (id != "amdin") {
+//                Toast.makeText(this, "아이디가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+////                3.비번 값 판별
+//            } else if (pwd != "asdf") {
+//                Toast.makeText(this, "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+////                4. 두가지 모두 맞을 때
+//            } else {
+//                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+//            }
 
             if(id == "admin" || pwd == "asdf") {
                 if(id != "admin") {
