34fbd7e1119f87c33b28f32fc873eb224a8036b2
shchoi <gllapia@naver.com>
2022-07-03 15:26:42 +0900

    회원가입 새창 추가 20220703 일

--- app/src/main/java/com/neppka/loginuipractice_20220703/MainActivity.kt
+++ app/src/main/java/com/neppka/loginuipractice_20220703/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppka.loginuipractice_20220703
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,44 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+//      회원 가입 창으로
         signUpBtn.setOnClickListener {
-            Toast.makeText(this, "회원가입버튼 클릭", Toast.LENGTH_SHORT).show()
-        }
+//            비행기 티켓을 발권(출발지 :this(MainActivity) -> 목적지(SignUpActivity)
+            val myIntent = Intent(this, SignUpActivity::class.java)
 
-        loginBtn.setOnClickListener {
-            Log.d("메인 액티비티", "로그인 버튼 클릭")
+//            실제 비행기를 출발
+            startActivity(myIntent) //myIntent : 비행기 티켓을 들고 타는 느낌
+
         }
 
 //        로그인 로직 실행
         loginBtn.setOnClickListener {
-//            아이디 값과 비번 값 변수화
-            val id = idEdt.text.toString()
-            val pwd = passEdt.text.toString()
-
-//        1. 아이디값이 "admin", 비번 "adsf"
-//        2. 아이디 값 판별
-//            if (id != "amdin") {
-//                Toast.makeText(this, "아이디가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-////                3.비번 값 판별
-//            } else if (pwd != "asdf") {
-//                Toast.makeText(this, "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-////                4. 두가지 모두 맞을 때
-//            } else {
-//                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
-//            }
-
-            if(id == "admin" || pwd == "asdf") {
-                if(id != "admin") {
-                    Toast.makeText(this, "아이디가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-                } else if(pwd != "asdf") {
-                    Toast.makeText(this, "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "로그인이 실패했습니다.", Toast.LENGTH_SHORT).show()
-            }
+            val myIntent2 = Intent(this, MainActivity2::class.java)
         }
     }
 }
